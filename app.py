35b518812e7239e2ef30a89c85c3be386eb53d0a
jeffzhou95 <xz6429@utexas.edu>
2019-07-17 22:12:19 -0500

    Add some layout to charts

--- app.py
+++ app.py
@@ -128,12 +128,10 @@ def pieChart1():
 
 	labels = 'Entertainment', 'Restaurant', 'Grocery', 'Transportation'
 	colors = ['gold', 'yellowgreen', 'lightcoral', 'lightskyblue']
-	explode = (0.1, 0, 0, 0)  # explode 1st slice
-
-	# Plot
-	plt.pie(sizes, explode=explode, labels=labels, colors=colors, autopct='%1.1f%%', shadow=True, startangle=140)
-
+	patches, texts = plt.pie(sizes, colors=colors, shadow=True, startangle=90)
+	plt.legend(patches, labels, loc="best")
 	plt.axis('equal')
+	plt.tight_layout()
 	plt.savefig('pieChart1.png')
 	plt.close()
 	connection.close()
