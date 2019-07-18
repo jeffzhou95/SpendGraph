aa7b32e1fbef2cdf219c6640848dda823e153704
jeffzhou95 <xz6429@utexas.edu>
2019-07-17 21:31:25 -0500

    complete two pie charts

--- app.py
+++ app.py
@@ -7,8 +7,6 @@ import sqlite3
 
 import json, datetime
 
-from item import Item
-
 # .\venv\Scripts\activate.bat
 
 app = Flask(__name__)
@@ -31,8 +29,16 @@ def foo():
 	# return render_template('index.html')
 	barChart()
 	data_uri = base64.b64encode(open('barChart.png', 'rb').read()).decode('utf-8')
-	img_tag = 'data:image/png;base64,{0}'.format(data_uri)
-	return render_template('country.html', zipCode=zipCode, tag=img_tag)
+	bar = 'data:image/png;base64,{0}'.format(data_uri)
+
+	pieChart()
+	data_uri = base64.b64encode(open('pieChart.png', 'rb').read()).decode('utf-8')
+	pie = 'data:image/png;base64,{0}'.format(data_uri)
+
+	pieChart1()
+	data_uri = base64.b64encode(open('pieChart1.png', 'rb').read()).decode('utf-8')
+	pie1 = 'data:image/png;base64,{0}'.format(data_uri)
+	return render_template('country.html', zipCode=zipCode, bar=bar, pie=pie, pie1=pie1)
 
 
 def barChart():
@@ -42,7 +48,6 @@ def barChart():
 	result = cursor.execute(query)
 
 	list = result.fetchall()
-	# print(list)
 
 	# amount of transactions split by ages
 	total = 0
@@ -57,15 +62,80 @@ def barChart():
 		y[index] = amt * 100 / total
 		index = index + 1
 
-
-
-	# y = [3, 10, 7, 5, 3, 4.5, 6, 8.1]
 	x = range(10, 100, 10)
 	width = 2
 	plt.bar(x, y, width, color="blue")
 	plt.xlabel('age')
 	plt.ylabel('percentage / %')
 	plt.savefig('barChart.png')
+	plt.close()
+	connection.close()
+
+def pieChart():
+	connection = sqlite3.connect("data.db")
+	cursor = connection.cursor()
+	query = "SELECT * FROM items"
+	result = cursor.execute(query)
+	list = result.fetchall()
+
+	total = 0
+	sizes = [0, 0]
+	for transaction in list:
+		total = total + transaction[3]
+		sizes[transaction[0]] = sizes[transaction[0]] + transaction[3]
+
+	sizes[0] = sizes[0] * 100 / total
+	sizes[1] = sizes[1] * 100 / total
+
+
+	labels = 'Man', 'Women'
+	colors = ['gold', 'lightskyblue']
+	explode = (0.1, 0)  # explode 1st slice
+
+	# Plot
+	plt.pie(sizes, explode=explode, labels=labels, colors=colors, autopct='%1.1f%%', shadow=True, startangle=140)
+
+	plt.axis('equal')
+	plt.savefig('pieChart.png')
+	plt.close()
+	connection.close()
+
+def pieChart1():
+	connection = sqlite3.connect("data.db")
+	cursor = connection.cursor()
+	query = "SELECT * FROM items"
+	result = cursor.execute(query)
+	list = result.fetchall()
+
+	total = 0
+	sizes = [0, 0, 0, 0]
+	for transaction in list:
+		total = total + transaction[3]
+		if transaction[2] == 'Entertainment' :
+			sizes[0] = sizes[0] + transaction[3]
+		elif transaction[2] == 'Restaurant' :
+			sizes[1] = sizes[1] + transaction[3]
+		elif transaction[2] == 'Grocery' :
+			sizes[2] = sizes[2] + transaction[3]
+		else:
+			sizes[3] = sizes[3] + transaction[3]
+
+	sizes[0] = sizes[0] * 100 / total
+	sizes[1] = sizes[1] * 100 / total
+	sizes[2] = sizes[2] * 100 / total
+	sizes[3] = sizes[3] * 100 / total
+
+
+	labels = 'Entertainment', 'Restaurant', 'Grocery', 'Transportation'
+	colors = ['gold', 'yellowgreen', 'lightcoral', 'lightskyblue']
+	explode = (0.1, 0, 0, 0)  # explode 1st slice
+
+	# Plot
+	plt.pie(sizes, explode=explode, labels=labels, colors=colors, autopct='%1.1f%%', shadow=True, startangle=140)
+
+	plt.axis('equal')
+	plt.savefig('pieChart1.png')
+	plt.close()
 	connection.close()
 
 if __name__ == "__main__":
